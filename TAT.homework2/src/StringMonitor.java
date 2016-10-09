c09fda8f69f8cc0c55ffc80c48e83e0f9ccace99
Yury Suponev <starosta130@gmail.com>
2016-10-09 16:14:37 +0300

    try to add file in repository

---
+++ TAT.homework2/src/StringMonitor.java
@@ -0,0 +1,32 @@
+/**
+ * Defines an entry point for the application, which determines whether a string satisfies the rules
+ * @author Yury Suponev
+ */
+public class StringMonitor {
+  /**
+   * Check the string
+   * @param str the string for check
+   */
+  private static void check(String str) {
+    DigitCheck digitCheck = new DigitCheck();
+    NotDigitCheck notDigitCheck = new NotDigitCheck();
+    VocabularyCheck vocabularyCheck = new VocabularyCheck();
+    NumberOfWordsCheck numberOfWordsCheck = new NumberOfWordsCheck();
+    digitCheck.checkString(str);
+    notDigitCheck.checkString(str);
+    vocabularyCheck.checkString(str);
+    numberOfWordsCheck.checkString(str);
+    if (!digitCheck.isSatisfied() && !notDigitCheck.isSatisfied() && !vocabularyCheck.isSatisfied()
+        && !numberOfWordsCheck.isSatisfied()) {
+      System.out.println("...doesn't satisfy any rule");
+    }
+  }
+
+  /**
+   * Reads a string from console
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+
+  }
+}
